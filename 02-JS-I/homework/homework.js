bc7cfd4dbf7834372a8157841b07e22452e771ed
Anabela <anabelahgonzalez@gmail.com>
2022-08-21 23:34:05 -0300

    Resuelto Homework 02-JS-I(arreglado el 35)

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -265,10 +265,11 @@ function esVocal(letra){
   if (letra.length>1){
     return "Dato incorrecto"
   }
-  else{
-    if (letra==='a') || (letra==='e') || (letra==='i') || (letra==='o') || (letra==='u') {
-      return "Es vocal"+"Dato Incorrecto"
+  else if ((letra==='a') || (letra==='e') || (letra==='i') || (letra==='o') || (letra==='u')) {
+      return "Es vocal"
     }
+      else {
+        return "Dato incorrecto"
       }
 }
 
